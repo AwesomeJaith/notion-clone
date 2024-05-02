d03ddeea3e01a08780e460ee3a3a0f969ae4d1d9
Jaith Darrah <generic.jaithd@gmail.com>
2024-05-02 17:00:06 -0500

    Add restore and delete buttons to trash button popout

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -18,6 +18,7 @@ import {
 import { UserItem } from "./user-item";
 import { Item } from "./item";
 import { DocumentList } from "./document-list";
+import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
     const pathname = usePathname();
@@ -171,7 +172,7 @@ export const Navigation = () => {
                             className="p-0 w-72"
                             side={isMobile ? "bottom" : "right"}
                         >
-                            <p>Trash box</p>
+                            <TrashBox />
                         </PopoverContent>
                     </Popover>
                 </div>
