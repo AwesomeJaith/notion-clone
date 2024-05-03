8d87df6807b8ece696dfe872341968c6df5f682a
Jaith Darrah <generic.jaithd@gmail.com>
2024-05-03 18:06:01 -0500

    Add document page editable title

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { ImageIcon, Smile, X } from "lucide-react";
+import { ElementRef, useRef, useState } from "react";
+import { useMutation } from "convex/react";
+import TextareaAutosize from "react-textarea-autosize";
 
 import { Doc } from "@/convex/_generated/dataModel";
 import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
 
 import { IconPicker } from "./icon-picker";
 
@@ -16,6 +20,41 @@ export const Toolbar = ({
     initialData,
     preview
 }: ToolbarProps) => {
+    const inputRef = useRef<ElementRef<"textarea">>(null);
+    const [isEditing, setIsEditing] = useState(false);
+    const [value, setValue] = useState(initialData.title);
+
+    const update = useMutation(api.documents.update);
+
+    const enableInput = () => {
+        if (preview) return;
+
+        setIsEditing(true);
+        setTimeout(() => {
+            setValue(initialData.title);
+            inputRef.current?.focus();
+        }, 0);
+    };
+
+    const disableInput = () => setIsEditing(false);
+
+    const onInput = (value: string) => {
+        setValue(value);
+        update({
+            id: initialData._id,
+            title: value || "Untitled"
+        });
+    };
+
+    const onKeyDown = (
+        event: React.KeyboardEvent<HTMLTextAreaElement>
+    ) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            disableInput();
+        }
+    };
+
     return (
         <div className="pl-[54px] group relative">
             {!!initialData.icon && !preview && (
@@ -55,7 +94,7 @@ export const Toolbar = ({
                 )}
                 {!initialData.coverImage && !preview && (
                     <Button
-                    onClick={() => {}}
+                        onClick={() => {}}
                         className="text-muted-foreground text-xs"
                         variant="outline"
                         size="sm"
@@ -65,6 +104,23 @@ export const Toolbar = ({
                     </Button>
                 )}
             </div>
+            {isEditing && !preview ? (
+                <TextareaAutosize
+                    ref={inputRef}
+                    onBlur={disableInput}
+                    onKeyDown={onKeyDown}
+                    value={value}
+                    onChange={(e) => onInput(e.target.value)}
+                    className="text-5xl bg-transparent font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"
+                />
+            ) : (
+                <div
+                    onClick={enableInput}
+                    className="pb-[11.5px text-5xl font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF]"
+                >
+                    {initialData.title}
+                </div>
+            )}
         </div>
     )
 }
