d066f4640a75b6a94d55643db034250d0ec537e8
Jaith Darrah <generic.jaithd@gmail.com>
2024-05-03 19:15:18 -0500

    Add change image button in header image

--- components/cover.tsx
+++ components/cover.tsx
@@ -3,6 +3,10 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
+import { Button } from "@/components/ui/button";
+import { ImageIcon, X } from "lucide-react";
+import { useCoverImage } from "@/hooks/use-cover-image";
+
 interface CoverImageProps {
     url?: string;
     preview?: boolean;
@@ -12,6 +16,8 @@ export const Cover = ({
     url,
     preview
 }: CoverImageProps) => {
+    const coverImage = useCoverImage();
+
     return (
         <div className={cn(
             "relative w-full h-[35vh] group",
@@ -25,6 +31,28 @@ export const Cover = ({
                     alt="Cover"
                     className="object-cover"
                 />
+            )}
+            {url && !preview && (
+                <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+                    <Button
+                        onClick={coverImage.onOpen}
+                        className="text-muted-foreground text-xs"
+                        variant="outline"
+                        size="sm"
+                    >
+                        <ImageIcon className="h-4 w-4 mr-2"/>
+                        Change cover
+                    </Button>
+                    <Button
+                        onClick={() => {}}
+                        className="text-muted-foreground text-xs"
+                        variant="outline"
+                        size="sm"
+                    >
+                        <X className="h-4 w-4 mr-2"/>
+                        Remove
+                    </Button>
+                </div>
             )}
         </div>
     )
